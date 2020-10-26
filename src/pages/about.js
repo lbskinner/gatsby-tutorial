e814a9a0e632caafbc114abdec1bdc1d06952668
Leila Skinner <beileiwang@gmail.com>
2020-10-26 16:31:48 -0500

    use page query for title for about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,12 @@
 import React from "react"
 import Header from "../components/header"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
-export default function About() {
+export default function About({ data }) {
   return (
     <Layout>
-      <Header headerText="About Pandas Eating Lots" />
+      <Header headerText={`About ${data.site.siteMetadata.title}`} />
       <p>
         We're the only site running on your computer dedicated to showing the
         best photos and videos of pandas eating lots of food.
@@ -13,3 +14,15 @@ export default function About() {
     </Layout>
   )
 }
+
+// Page queries live outside of the component definition — by convention
+// at the end of a page component file — and are only available on page components.
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
