a96b91e53496206eff41c6d0739347a161da21ac
Leila Skinner <beileiwang@gmail.com>
2020-11-02 15:44:26 -0600

    created a template directory for blog post in src and update gatsbu node file to create pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -13,6 +14,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   }
 
   exports.createPages = async ({ graphql, actions }) => {
+      const { createPage } = actions
     // **Note:** The graphql function call returns a Promise
     // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
     const result = await graphql(`
@@ -28,5 +30,16 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
         }
       }
     `)
-    console.log(JSON.stringify(result, null, 4))
+    
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+          path: node.fields.slug,
+          component: path.resolve(`./src/templates/blog-post.js`),
+          context: {
+            // Data passed to context is available
+            // in page queries as GraphQL variables.
+            slug: node.fields.slug,
+          },
+        })
+      })
   }
